dc1b556de6cb9f82f95e4c3679a64e7276e36a02
Swerad <lukasz.sitnik@gmail.com>
2019-10-20 16:00:12 +0200

    Created a basic application

---
+++ app.js
@@ -0,0 +1,27 @@
+const request = require("request");
+const fs = require("fs");
+const validCodes = ["usd", "gbp", "eur", "chf"];
+const code = process.argv[2];
+const isValid = validCodes.find(currency => currency === code) ? true : false;
+const url = `http://api.nbp.pl/api/exchangerates/rates/a/${code}/?format=json`;
+
+request(url, {
+  json: true
+}, (err, res, body) => {
+  setCorrectGrammar(body);
+  console.log(`Kurs ${body.currency} w dniu ${body.rates[0].effectiveDate} wynosi ${body.rates[0].mid}`);
+});
+
+const setCorrectGrammar = body => {
+  switch (process.argv[2]) {
+    case "usd" || "USD":
+      body.currency = "dolara amerykańskiego";
+      break;
+    case "gbp" || "GBP":
+      body.currency = "funta szterlinga";
+      break;
+    case "chf" || "CHF":
+      body.currency = "franka szwajcarskiego";
+      break;
+  };
+};
